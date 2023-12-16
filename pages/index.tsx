c8c71f245e6d01418723165878db5a5ea2c76c5a
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2023-12-16 17:05:00 +1100

    Added redux into the project using provider context api, created sample store and a slice for a task

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from "react"
 
 const HomePage: React.FC = () => {
-  return (
-    <div>
-      <h1>My Next.js Application with TypeScript and Redux</h1>
-      <h2>Created by Manijeh Shirzadeh</h2>
-      <h3>Now it's connected to Vercel Build and Deploy</h3>
-    </div>
-  );
-};
+	return (
+		<div>
+			<h1>My Next.js Application with TypeScript and Redux</h1>
+			<h2>Created by Manijeh Shirzadeh</h2>
+			<h3>Now it's connected to Vercel Build and Deploy</h3>
+		</div>
+	)
+}
 
-export default HomePage;
+export default HomePage
---
+++ store/taskSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+
+interface Task {
+	id: number
+	name: string
+}
+
+interface TaskState {
+	tasks: Task[]
+}
+
+const initialState: TaskState = {
+	tasks: []
+}
+
+export const taskSlice = createSlice({
+	name: "task",
+	initialState,
+	reducers: {
+		addTask: (state, action: PayloadAction<string>) => {
+			const newTask: Task = {
+				id: state.tasks.length + 1,
+				name: action.payload
+			}
+			state.tasks.push(newTask)
+		},
+		removeTask: (state, action: PayloadAction<number>) => {
+			state.tasks = state.tasks.filter(task => task.id !== action.payload)
+		}
+	}
+})
+
+export const { addTask, removeTask } = taskSlice.actions
+
+export default taskSlice.reducer
