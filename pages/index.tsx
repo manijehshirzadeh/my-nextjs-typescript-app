4134bb6a2b7ecc43504a38a767210e78d2ad285d
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2023-12-16 15:48:15 +1100

    Initilizade the app with compatible packages, initilizade git and added gitignore file.

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>My Next.js Application with TypeScript and Redux</h1>
+      <h2>Created by Manijeh Shirzadeh</h2>
+    </div>
+  );
+};
+
+export default HomePage;
