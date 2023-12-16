c6d74cdb1658238562cadb641f31e487b45fd88f
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2023-12-16 17:14:53 +1100

    Created a Task List Component and connected it to store, changed the html elements to be all MUI components

---
+++ components/MyAppComponent.tsx
@@ -0,0 +1,115 @@
+import React from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { RootState, AppDispatch } from "../store"
+import { addTask, removeTask } from "../store/taskSlice"
+import { Formik, Field, Form, ErrorMessage, FormikHelpers } from "formik"
+import * as Yup from "yup"
+import {
+	Button,
+	TextField,
+	List,
+	ListItem,
+	Typography,
+	Container,
+	Box,
+	IconButton,
+	ListItemAvatar,
+	Avatar,
+	ListItemText
+} from "@mui/material"
+import DeleteIcon from "@mui/icons-material/Delete"
+import WorkIcon from "@mui/icons-material/Work"
+
+interface FormValues {
+	taskName: string
+}
+
+interface Task {
+	id: number
+	name: string
+}
+
+const validationSchema = Yup.object({
+	taskName: Yup.string().required("Task name is required")
+})
+
+const MyAppComponent: React.FC = () => {
+	const dispatch = useDispatch<AppDispatch>()
+	const tasks = useSelector((state: RootState) => state.task.tasks)
+
+	const initialValues: FormValues = {
+		taskName: ""
+	}
+
+	const handleSubmit = (
+		values: FormValues,
+		{ resetForm }: FormikHelpers<FormValues>
+	) => {
+		dispatch(addTask(values.taskName))
+		resetForm()
+	}
+
+	const handleRemoveTask = (taskId: number) => {
+		dispatch(removeTask(taskId))
+	}
+
+	return (
+		<Container>
+			<Box mt={4}>
+				<Typography variant="h4" gutterBottom>
+					Task List
+				</Typography>
+				<List dense>
+					{tasks.map((task: Task) => (
+						<ListItem
+							secondaryAction={
+								<IconButton
+									onClick={() => handleRemoveTask(task.id)}
+									edge="end"
+									aria-label="delete"
+								>
+									<DeleteIcon />
+								</IconButton>
+							}
+						>
+							<ListItemAvatar>
+								<Avatar>
+									<WorkIcon />
+								</Avatar>
+							</ListItemAvatar>
+							<ListItemText primary={task.name} />
+						</ListItem>
+					))}
+				</List>
+				<Formik
+					initialValues={initialValues}
+					validationSchema={validationSchema}
+					onSubmit={handleSubmit}
+				>
+					<Form>
+						<Box>
+							<Field
+								as={TextField}
+								type="text"
+								id="taskName"
+								name="taskName"
+								label="Task Name"
+								variant="outlined"
+								fullWidth
+								margin="normal"
+							/>
+							<ErrorMessage name="taskName" component="Box" />
+						</Box>
+						<Box>
+							<Button type="submit" variant="contained" color="primary">
+								Add Task
+							</Button>
+						</Box>
+					</Form>
+				</Formik>
+			</Box>
+		</Container>
+	)
+}
+
+export default MyAppComponent
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
+import MyAppComponent from "@components/MyAppComponent"
+import { Box, Typography } from "@mui/material"
 import React from "react"
 
 const HomePage: React.FC = () => {
 	return (
-		<div>
-			<h1>My Next.js Application with TypeScript and Redux</h1>
-			<h2>Created by Manijeh Shirzadeh</h2>
-			<h3>Now it's connected to Vercel Build and Deploy</h3>
-		</div>
+		<Box display="flex" flexDirection="column" alignItems="center">
+			<Typography variant="h2">
+				My Next.js Application with TypeScript and Redux
+			</Typography>
+			<Typography variant="h4">Created by Manijeh Shirzadeh</Typography>
+			<Typography variant="h5">
+				Now it's connected to Vercel Build and Deploy
+			</Typography>
+			<MyAppComponent />
+		</Box>
 	)
 }
 
