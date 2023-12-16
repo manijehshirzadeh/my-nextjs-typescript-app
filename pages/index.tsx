f762ddd40b5baf07f99a429718928faacf96d3fb
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2023-12-16 16:34:47 +1100

    Connecting into vercel for build and deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ const HomePage: React.FC = () => {
     <div>
       <h1>My Next.js Application with TypeScript and Redux</h1>
       <h2>Created by Manijeh Shirzadeh</h2>
+      <h3>Now it's connected to Vercel Build and Deploy</h3>
     </div>
   );
 };
